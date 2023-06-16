10ee7a35efe127e9c4595a9c6d7e4dd0d8a01a9c
eduardofloriani <eeduardofloriani@gmail.com>
2023-06-16 17:04:29 -0300

    1:55

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { CarProps } from "@/types";
 import CustomButton from "./CustomButton";
 import { calculteCarRent } from "@/utils";
+import CarDetails from "./CarDetails";
 
 interface CarCardProps {
     car: CarProps
@@ -36,7 +37,7 @@ const CarCard = ({ car }: CarCardProps) => {
             </p>
 
             <div className="relative w-full h-40 my-3 object-contain">
-                <Image src='/hero.png' alt="car model" fill priority 
+                <Image src='/hero.png' alt="car model" fill priority
                     className="object-contain" />
             </div>
 
@@ -74,6 +75,12 @@ const CarCard = ({ car }: CarCardProps) => {
                     />
                 </div>
             </div>
+
+            <CarDetails 
+                isOpen={isOpen}
+                closeModal={() => setIsOpen(false)}
+                car={car}
+            />
         </div>
     )
 }
